b54f3753a2543f25de373bcff0057023ec466b52
sebasrpg1232 <sebasrpg1232@gmail.com>
2023-03-24 20:53:54 -0500

    Arreglo

--- tienda-tenis/src/app/app.module.ts
+++ tienda-tenis/src/app/app.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { LoginComponent } from './login/login.component';
 
-const appRoutes:Routes=[
-{path:'',component:AppComponent},
-{path:'login',component:LoginComponent}
-
-];
 
 @NgModule({
   declarations: [
@@ -20,8 +13,7 @@ const appRoutes:Routes=[
     LoginComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
