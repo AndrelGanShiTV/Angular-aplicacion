13a7831830bba57c66e764f6fc1db30123fa8bef
Anderson23j <andersonvalenciaj23@gmail.com>
2023-03-24 20:26:40 -0500

    componente nuevo

--- tienda-tenis/src/app/app.module.ts
+++ tienda-tenis/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes:Routes=[
+{path:'',component:AppComponent},
+{path:'login',component:LoginComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
