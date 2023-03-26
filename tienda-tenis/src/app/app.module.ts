69856797105f578d9882a9b78021bfd1e449a84b
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-03-26 15:45:51 -0500

    Borrar Path Productos

--- tienda-tenis/src/app/app.module.ts
+++ tienda-tenis/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrarComponent } from './components/registrar/registrar.component';
 
@@ -10,7 +9,6 @@ import { RegistrarComponent } from './components/registrar/registrar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
     LoginComponent,
     RegistrarComponent
   ],
